0af78dc3a79d2c42676690f18a7c88c26cd75972
cpf <643842536@qq.com>
2018-09-01 13:37:08 +0800

    create app:home

---
+++ Blog/apps/home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from ..posts.models import Post
+# Create your views here.
+
+
+def home(request):
+	'''
+	博客网站首页视图
+	'''
+
+	posts = Post.objects.all()
+
+	#往模板中传递的上下文
+	context = {
+		'posts':posts
+	}
+	return render(request,'home.html',context)
