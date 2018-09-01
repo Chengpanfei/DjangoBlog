4adcf1b4000c4ab0437bf82f1111179a926f8b32
cpf <643842536@qq.com>
2018-09-01 17:07:30 +0800

    add templates for home and modify the settings for static files

--- Blog/settings.py
+++ Blog/settings.py
@@ -118,3 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = (  
+    os.path.join(BASE_DIR,'Blog/static'),
+)
+ 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
