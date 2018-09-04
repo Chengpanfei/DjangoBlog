16da160cdb6caf05d2872da420e6977ee82c9827
cpf <643842536@qq.com>
2018-09-04 22:16:57 +0800

    Change the home template

--- Blog/apps/home/views.py
+++ Blog/apps/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count
 from ..posts.models import Post
 # Create your views here.
 
@@ -7,12 +8,16 @@ def home(request):
 	'''
 	博客网站首页视图
 	'''
+	#按照时间排序，取最近3个博文
+	lasted_posts = Post.objects.all().order_by('-create_time')[0:3]
+	#按照拥有的评论数目排序，聚合后的结果作为属性
+	hot_posts = Post.objects.annotate(comment_num=Count('comments')).order_by('-comment_num')[0:3]
 
-	posts = Post.objects.all()[1:]
 
 	#往模板中传递的上下文
 	context = {
-		'posts':posts
+		'lasted_posts':lasted_posts,
+		'hot_posts':hot_posts,
 	}
 	return render(request,'home.html',context)
 
