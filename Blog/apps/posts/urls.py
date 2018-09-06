cfb134d525931be9e2baac0d0376a82b5c06f003
cpf <643842536@qq.com>
2018-09-06 12:52:45 +0800

    Add post create view

--- Blog/apps/posts/urls.py
+++ Blog/apps/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import (
-	post_content
+	post_content,
+	post_create,
 	)
 
 urlpatterns = [
     path('<int:post_id>/', post_content),
+    path('create/',post_create),
 ]
--- Blog/apps/posts/views.py
+++ Blog/apps/posts/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.http import Http404
 from .models import Post
+from ..categories.models import Category
+
+from .forms import PostForm
+
+
+from django.contrib.auth.decorators import (
+		login_required,
+		permission_required
+		)
 # Create your views here.
 
 def post_content(request,post_id):
@@ -23,3 +32,32 @@ def post_content(request,post_id):
 	}
 
 	return render(request,'post_content.html',context)
+
+
+'''
+先判断是否登录，若没有登录将会重定向到登录页面
+再判断是否有相关权限，若没有将返回403 Forbidden
+'''
+@login_required(login_url='/users/login/')
+@permission_required('Blog.add_post',raise_exception=True)
+def post_create(request):
+	
+	if request.method == 'POST':
+
+		form  = PostForm(request.POST)
+		if form.is_valid:
+
+
+			title = request.POST['title'] 
+			body = request.POST['body']
+			category = request.POST['category']
+
+			category = get_object_or_404(Category,name=category)
+			
+
+			post = Post(title=title,category=category,content=body,author=request.user)
+
+			post.save()
+			return redirect('/')
+	categories = Category.objects.all()
+	return render(request,'posts_create.html',{'categories':categories})
