aa2b7e3318b1d89f7aced2444d95b5edc2f4b85b
cpf <643842536@qq.com>
2018-09-10 12:15:03 +0800

    fix a bug of static_root

--- Blog/settings.py
+++ Blog/settings.py
@@ -121,7 +121,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/root/Django/DjangoBlog/'
+STATIC_ROOT = '/root/Django/DjangoBlog/static/'
 
 STATICFILES_DIRS = (  
     os.path.join(BASE_DIR,'Blog/static'),
